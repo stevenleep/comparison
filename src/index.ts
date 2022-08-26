29d97367288908db0e4e979806dbeb84f052e779
Branlice <lishiwen@wclound.cn>
2022-08-26 16:07:19 +0800

    chore: integrated typedoc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import Parser from "./Parser";
+
 function JS_SDK(context: string) {
     console.log('JSSDK', context);
 }
 
 JS_SDK('hello');
+const parser = new Parser();
+
+console.log(parser);
+
 export default JS_SDK;
